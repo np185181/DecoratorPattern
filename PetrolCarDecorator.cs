813b98ecbec1c64bb5d45fc4bf7556d35e488481
Navratna Pawale <np185181@ncr.com>
2022-11-21 11:42:53 +0530

    Add project files.

---
+++ PetrolCarDecorator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DecoratorDesignPattern
+{
+    public class PetrolCarDecorator : CarDecorator
+    {
+        public PetrolCarDecorator(ICar car) : base(car)
+        {
+        }
+
+        public override ICar ManufactureCar()
+        {
+            car.ManufactureCar();
+            AddEngine(car);
+            return car;
+        }
+
+        public void AddEngine(ICar car)
+        {
+            if (car is BMW)
+            {
+                BMW BMWCar = (BMW)car;
+                BMWCar.Engine = "Petrol Engine";
+                Console.WriteLine("PetrolCarDecorator added Petrol Engine to the Car : " + car);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DecoratorDesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ICar bmwCar1 = new BMW();
+            bmwCar1.ManufactureCar();
+            Console.WriteLine(bmwCar1 + "\n");
+            DieselCarDecorator carWithDieselEngine = new DieselCarDecorator(bmwCar1);
+            carWithDieselEngine.ManufactureCar();
+            Console.WriteLine();
+
+            ICar bmwCar2 = new BMW();
+            PetrolCarDecorator carWithPetrolEngine = new PetrolCarDecorator(bmwCar2);
+            carWithPetrolEngine.ManufactureCar();
+            Console.ReadKey();
+        }
+    }
+}
